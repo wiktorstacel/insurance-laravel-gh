861cbd2a1a2e8d250a13322f48428b55dcf4be0e
wiktorstacel <stawik964@gmail.com>
2021-03-31 20:41:53 +0200

    Edit data and update MySQL table after

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -14,7 +14,8 @@ class StudentController extends Controller
      */
     public function index()
     {
-        //
+        $students = Student::all()->toArray();
+        return view('student.index', compact('students'));
     }
 
     /**
@@ -44,7 +45,7 @@ class StudentController extends Controller
             'last_name'     =>  $request->get('last_name')
         ]);
         $student->save();
-        return redirect()->route('student.create')->with('success', 'Data Added');
+        return redirect()->route('student.index')->with('success', 'Data Added');//create changed to index
     }
 
     /**
@@ -66,7 +67,8 @@ class StudentController extends Controller
      */
     public function edit($id)
     {
-        //
+        $student = Student::find($id);
+        return view('student.edit', compact('student', 'id'));
     }
 
     /**
@@ -78,7 +80,15 @@ class StudentController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'first_name'    =>  'required',
+            'last_name'     =>  'required'
+        ]);
+        $student = Student::find($id);
+        $student->first_name = $request->get('first_name');
+        $student->last_name = $request->get('last_name');
+        $student->save();
+        return redirect()->route('student.index')->with('success', 'Data Updated');
     }
 
     /**
