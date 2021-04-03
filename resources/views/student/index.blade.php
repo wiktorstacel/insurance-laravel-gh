44864449210d63d41b6962e1ed53e415d842f85b
wiktorstacel <stawik964@gmail.com>
2021-04-03 14:49:16 +0200

    Delete row of the MySQL table functionality

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -29,11 +29,30 @@
                 <td>{{ $row['first_name'] }}</td>
                 <td>{{ $row['last_name'] }}</td>
                 <td><a href="{{action('StudentController@edit', $row['id'])}}" class="btn btn-warning">Edit</a></td>
-                <td></td>
+                <td>
+                    <form method="post" class="delete_form" action="{{action('StudentController@destroy', $row['id'])}}">
+                        {{csrf_field()}}
+                        <input type="hidden" name="_method" value="DELETE" />
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
             </tr>
             @endforeach
         </table>
     </div>
 </div>
-
+<script>
+$(document).ready(function(){
+    $('.delete_form').on('submit', function(){
+        if(confirm("Are you sure you want to delete it?"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    })
+});
+</script>
 @endsection
