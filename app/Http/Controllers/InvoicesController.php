cbd282e8fd4236be731d0a8389220d0254f2e648
wiktorstacel <stawik964@gmail.com>
2025-01-04 17:15:50 +0100

    Usage of Eloquent ORM to save object Invoice to mysql

--- app/Http/Controllers/InvoicesController.php
+++ app/Http/Controllers/InvoicesController.php
@@ -34,14 +34,20 @@ class InvoicesController extends Controller
             'date'   => 'required|max:15',
             'total'   =>  'required|numeric|min:1'
         ]);
-        $invoice = new Invoice();
-        
-        $invoice->number = $request->number;
+        $invoice = new Invoice(); //do zmiennej $invoice przypisz nowy "Model Invoice" - tabela invoices i jej pola zdefiniowana w migracjach
+        //Klasa Invoice dziedziczy po klasie Model, która jest częścią Eloquent ORM
+
+        $invoice->numbers = $request->number; //$invoice->numbers - nazwa pola taka ja w bazie Mysql
         $invoice->date = $request->date;
         $invoice->total = $request->total;
         
-        $invoice->save();
-        
+        $invoice->save(); //Metoda save() jest jedną z metod zdefiniowanych w klasie Model. Służy do zapisywania obiektu (rekordu) do bazy danych.        
+        //Eloquent ORM wykorzystuje Active Record – wzorzec projektowy, który mapuje obiekt PHP na tabelę w bazie danych.
+        //Laravel automatycznie:
+        //Dopasowuje tabelę do modelu (np. model Invoice jest powiązany z tabelą invoices).
+        //Mapuje kolumny tabeli na atrybuty obiektu (np. kolumny numbers, date, total są przypisane do $invoice->numbers, $invoice->date, $invoice->total).
+
+
         return redirect()->route('invoices.index')->with('message', 'Faktura dodana poprawnie.');
     }
     
